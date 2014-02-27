8d35cb43bc7775d242b7109d7d4352130028763d
Daniel Szmulewicz <danielsz@sdf.lonestar.org>
2014-02-27 23:21:53 +0200

    description in project.clj

--- project.clj
+++ project.clj
@@ -1,15 +1,14 @@
-(defproject etsy "0.0.1"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject etsy "0.0.2"
+  :description "Etsy API library for Clojure"
+  :url "https://github.com/danielsz/etsy-clojure-api"
   :license {:name "Eclipse Public License"
-            :url "https://github.com/danielsz/etsy-clojure-api"}
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http "0.7.9"]
                  [clj-oauth "1.4.1"]
                  [org.clojure/data.json "0.2.4"]]
   :repositories [["releases" {:url "https://clojars.org/" :creds :gpg}]]
   :scm {:name "git"
-         :url "https://github.com/danielsz/etsy-clojure-api"}
-  :main ^:skip-aot etsy.core
+        :url "https://github.com/danielsz/etsy-clojure-api"}
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
