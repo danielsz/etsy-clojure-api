a24a91d754b2c3bd7b72faec23cd5ca77bc9ca03
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2014-09-25 02:23:08 +0300

    latest dependencies

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject etsy "0.1.0"
+(defproject etsy "0.1.1"
   :description "Etsy API library for Clojure"
   :url "https://github.com/danielsz/etsy-clojure-api"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "0.7.9"]
-                 [clj-oauth "1.4.1"]
-                 [org.clojure/data.json "0.2.4"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-http "1.0.0"]
+                 [clj-oauth "1.5.1"]
+                 [org.clojure/data.json "0.2.5"]]
   :repositories [["releases" {:url "https://clojars.org/" :creds :gpg}]
                  ["snapshots" {:url "https://clojars.org/" :creds :gpg}]]
   :scm {:name "git"
