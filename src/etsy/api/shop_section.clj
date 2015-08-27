0163f515aded3f08b41c6029f3f7696147926513
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2015-08-27 14:45:12 +0200

    shop sections

--- src/etsy/api/shop_section.clj
+++ src/etsy/api/shop_section.clj
@@ -2,3 +2,9 @@
   (:require [etsy.client :refer [api-call]]))
 
 ;; https://www.etsy.com/developers/documentation/reference/shopsection
+
+(defn find-all-shop-sections [shop-id]
+  "Retrieves a set of ShopSection objects associated to a Shop."
+  (api-call :GET (str "/shops/" shop-id "/sections")))
+
+
