e6843c52439df24c05571807170f85a4f9071c5c
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2015-10-15 00:15:38 +0300

    get-shop-section and find-all-shop-section-lisitngs-active

--- src/etsy/api/listing.clj
+++ src/etsy/api/listing.clj
@@ -12,3 +12,8 @@
   "Get listing by listing id"
   [listing-id]
   (api-call :GET (str "/listings/" listing-id)))
+
+(defn find-all-shop-section-listings-active
+  "Finds all listings within a shop section"
+  [shop-id shop_section_id & {:keys [limit offset page sort_on sort_order] :as params}]
+  (api-call :GET (str "/shops/" shop-id "/sections/" shop_section_id "/listings/active") :params params))
--- src/etsy/api/shop_section.clj
+++ src/etsy/api/shop_section.clj
@@ -7,4 +7,7 @@
   "Retrieves a set of ShopSection objects associated to a Shop."
   (api-call :GET (str "/shops/" shop-id "/sections")))
 
+(defn get-shop-section [shop-id section-id]
+  "Retrieves a ShopSection by id and shop_id"
+  (api-call :GET (str "/shops/" shop-id "/sections/" section-id)))
 
