39bd6ec06102b2877c70afc580ead492194e2b09
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2020-08-31 03:42:28 +0300

    throttled client

--- src/etsy/core.clj
+++ src/etsy/core.clj
@@ -1,4 +1,5 @@
-(ns etsy.core)
+(ns etsy.core
+  (:require [sada-abe.constant-rate-alt :refer [throttle-fn]]))
 
 (def consumer (atom {}))
 
@@ -8,12 +9,14 @@
 (defn make-consumer [key secret]
   (reset! consumer {:key key :secret secret}))
 
-(defn callfn [f & args] (apply f args))
-
-(defn make-client [consumer-key consumer-secret]
+(defn make-client
+  [consumer-key consumer-secret & {:keys [throttled?] :or {throttled? false}}]
   (make-consumer consumer-key consumer-secret)
-  callfn)
-  
+  (if throttled?
+    (fn [f & args] (let [f* (throttle-fn f 10 :second)]
+                     (apply f* args)))
+    (fn [f & args] (apply f args))))
+
 (defmacro with-user
   "Sets the user OAuth access token for write access and for accessing private user data."
   [oauth-token oauth-secret & body]
